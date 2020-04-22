3ee48b090098f5f47a38d61cc62eaefaec3f1219
Taylor Otwell <taylorotwell@gmail.com>
2020-04-22 09:01:24 -0500

    add service provider

---
+++ src/LegacyFactoryServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Illuminate\Database;
+
+use Faker\Generator;
+use Illuminate\Support\ServiceProvider;
+
+class LegacyFactoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Factory::class, function ($app) {
+            return Factory::construct(
+                $app->make(Generator::class), $this->app->databasePath('factories')
+            );
+        });
+    }
+}
